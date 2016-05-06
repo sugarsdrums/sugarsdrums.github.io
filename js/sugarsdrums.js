64e185df2cf3e9f96d429ddd48de6684a739a94d
fukaiyas <yasuyuki.fukai@gmail.com>
2016-05-06 18:42:13 +0900

    プロフィールを作成

--- js/sugarsdrums.js
+++ js/sugarsdrums.js
@@ -6,4 +6,13 @@ jQuery(function($){
   $.get("footer.html", function(data){
     $("footer").html(data);
   });
+  
+  //イメージ
+  $(".clickableimage").click(function(){
+    var $orgtag = $(this), src = $orgtag.attr("src"), $fl = $("<div id='filter'><img src='" + src + "'/></div>");
+    $fl.click(function(){
+      $(this).remove();
+    });
+    $("body").append($fl);
+  });
 });
